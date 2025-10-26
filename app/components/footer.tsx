0d62f29dac7c26472f3b24ed9518db1521f8226f
Aya Husein <prime4@techsyria.com>
2025-10-26 14:01:52 +0300

    v1

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -5,9 +5,9 @@ import Image from "next/image";
 export default function Footer() {
   return (
     <footer className="bg-[#002A26] py-8 shadow-inner">
-      <div className="container mx-auto flex flex-col justify-center items-center text-center space-y-3">
+      <div className="container mx-auto flex flex-col items-start text-left space-y-2 px-6">
         {/* النص فوق اللوغو */}
-        <p className="text-[#B19766] text-sm tracking-wide uppercase font-semibold">
+        <p className="text-[#B19766] text-xs tracking-wider uppercase font-medium">
           Powered by <span className="text-white">Prime Tech Solution</span>
         </p>
 
@@ -16,8 +16,8 @@ export default function Footer() {
           <Image
             src="/logo1.png"
             alt="Prime Tech Logo"
-            width={120}
-            height={80}
+            width={50}
+            height={30}
             className="object-contain transition-transform duration-300 hover:scale-110"
             priority
           />
